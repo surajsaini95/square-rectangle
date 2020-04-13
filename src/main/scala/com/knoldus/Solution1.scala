df6381e34fac86eb01ea9daf632611357c10acee
Suraj Saini <surajkumar90@rocketmail.com>
2020-04-13 17:38:19 +0530

    updated for CI

--- src/main/scala/com/knoldus/Solution1.scala
+++ src/main/scala/com/knoldus/Solution1.scala
@@ -5,8 +5,11 @@ object Solution1 extends App {
 
   val length = 11
   val breadth = 3
-  val rectangle: FourSidedShape = new Rectangle(length, breadth)
-  val areaOfRectangle = rectangle.getShapeArea
+  val rectangle1: FourSidedShape = new Rectangle(length, breadth)
+  val areaOfRectangle1 = rectangle1.getShapeArea
+
+  val rectangle2: FourSidedShape = new Rectangle(length, breadth)
+  val areaOfRectangle2 = rectangle2.getShapeArea
 
   val side = 5
   val square: FourSidedShape = new Square(side)
