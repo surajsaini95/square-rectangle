779c7d3c391fe488ffe0c0fc01fa403e774f88c7
Suraj Saini <surajkumar90@rocketmail.com>
2020-04-13 16:09:38 +0530

    Update Solution1.scala

--- src/main/scala/com/knoldus/Solution1.scala
+++ src/main/scala/com/knoldus/Solution1.scala
@@ -3,12 +3,12 @@ package com.knoldus
 
 object Solution1 extends App {
 
-  val length = 10
-  val breadth = 2
+  val length = 11
+  val breadth = 3
   val rectangle: FourSidedShape = new Rectangle(length, breadth)
   val areaOfRectangle = rectangle.getShapeArea
 
-  val side = 3
+  val side = 5
   val square: FourSidedShape = new Square(side)
 
   val areaOfSquare = square.getShapeArea
